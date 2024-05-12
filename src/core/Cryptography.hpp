5ed7f4cbb1ffdd4070a346216cddc435523c5047
VladimirKrk <vlkgroupyt@gmail.com>
2024-05-12 21:59:57 +0300

    Cryptography changes

---
+++ src/core/Cryptography.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+
+
+#include <sstream>
+#include <iomanip>
+
+#include <openssl/sha.h>
+#include <openssl/ec.h>
+#include <openssl/ecdh.h>
+#include <openssl/ripemd.h>
+
+#include <chrono>
+#include <thread>
+#include "spdlog/spdlog.h"
+#include "MerkleTree.hpp" //from Merkle to MerkleTree
+
+namespace Utils
+{
+	//struct _KEYPAIR;
+	//void ECOH(KEYPAIR* current, EC_GROUP* ec_group);
+	std::string sha256(const std::string str);
+	//std::string hextoascii(const std::string& hex);
+	std::string merkleMe(const std::string& s1);
+	std::string merkleMeAgain(const std::string& s1, const std::string& s2);
+	std::string getMerkleRoot(std::vector<std::string>& txs);
+	merkle::Hash getMerkleRootM(std::vector<std::string>& txs);
+	std::pair<std::string, std::string> findHash(int dificulty, int index, std::string prevHash, std::vector<std::string>& merkle);
+
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "core/Block.hpp"
 #include "core/Miner.hpp"
 #include "libfort/lib/fort.hpp"
+//Testing new libs
+#include "core/Cryptography.hpp"
 
 int main(void) {
+     // Test SHA-256 hash function
+    std::string sha256_hash = Utils::sha256("Hello, World!");
+    std::cout << "SHA-256 Hash: " << sha256_hash << std::endl;
+    
     auto server = std::make_shared<HttpServer>();
     auto miner = std::make_unique<Miner>(server);
 
