1758798e3927029f6d73b42ebd2938b91392bd6c
mcmonke <fera.cz@mail.ru>
2024-05-14 16:40:38 +0300

    ‘Transaction’

---
+++ src/core/Transaction.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../json/single_include/nlohmann/json.hpp"
+#include <openssl/lhash.h>
+
+class Transaction
+{
+public:
+	static constexpr uint32_t INDEX_NULL = std::numeric_limits<uint32_t>::max();
+	int16_t version = 0;
+private:
+	float amount;
+	std::unique_ptr<std::string> in;
+	std::unique_ptr<std::string> out;
+	uint64_t hash;
+
+public:
+	Transaction() = default;
+
+	Transaction(const Transaction&) = default;
+	Transaction& operator=(const Transaction&) = default;
+
+	Transaction(float amount, std::unique_ptr<std::string> out, std::unique_ptr<std::string> in)
+		:
+		amount(amount),
+		in(std::move(in)),
+		out(std::move(out)) {}
+
+
+public:
+	uint64_t calculateHash() const {};
+	const uint64_t getHash() const { return hash; }
+
+};
